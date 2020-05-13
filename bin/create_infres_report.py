c3bccf2e4cb571b97fd3187e039f04737ab6d0f5
saramonzon <sara.monzon.fdz@gmail.com>
2020-05-13 13:12:09 +0200

    added logging

--- bin/create_infres_report.py
+++ bin/create_infres_report.py
@@ -4,6 +4,7 @@ import json
 import re
 import os
 import argparse
+import datetime
 
 logger = logging.getLogger()
 
@@ -22,12 +23,41 @@ def get_arguments():
 
         return arguments
 
-    args = get_arguments()
 
-    input_dir = os.path.abspath(args.input_dir)
 
 
 def main():
+    # get arg
+    args = get_arguments()
+
+    #Create log file with date and time
+    right_now = str(datetime.date.today())
+    right_now_full = "_".join(right_now.split(" "))
+
+    log_filename = 'infres_creation' + "_" + right_now_full + ".log"
+    log_full_path = os.path.join(args.output, log_filename)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s:%(message)s')
+
+    file_handler = logging.FileHandler(log_full_path)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    #stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+
+    #####################START PIPELINE################
+
+    logger.info(args)
+
     #############FROM API###############################
     #url= 'https://flavia.isciii.es/api/'
     #response = requests.get(url)
@@ -41,14 +71,17 @@ def main():
     with open(args.json, 'r') as service:
         try:
             variables=json.load(service)
-        except ValueError:
-            print('Decoding JSON has failed')
+        except ValueError as e:
+            logger.error("json decoding has failed")
+            logger.exception(e)
+
     #print(variables)
     outputText = template.render(variables)
     file = open(args.output + "/INFRES_"+variables['SERVICE_NUMBER']+".md","wb")
     file.write(outputText.encode("utf-8"))
     file.close()
 
+    logger.info("Infres has being successfully rendered.")
 
 if __name__ == '__main__':
     try:
