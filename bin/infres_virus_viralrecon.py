bde77c2ecf911ab29d80371b01276629e7437c0a
saramonzon <sara.monzon.fdz@gmail.com>
2020-05-13 13:02:07 +0200

    script modifications for accepting parameters

--- bin/infres_virus_viralrecon.py
+++ bin/infres_virus_viralrecon.py
@@ -3,26 +3,56 @@ import jinja2
 import json
 import re
 import os
+import argparse
+
+logger = logging.getLogger()
 
 ###########JINJA2 READER#########
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
-templateEnv = jinja2.Environment(loader=templateLoader)
-TEMPLATE_FILE = "template/INFRES_VIRUS_VIRALRECON.j2"
-template = templateEnv.get_template(TEMPLATE_FILE)
+TEMPLATE_FILE = "templates/buisciii-infres.j2"
 
-#############FROM API###############################
-#url= 'https://flavia.isciii.es/api/'
-#response = requests.get(url)
-#variables = json.loads(response.text)
 
-##################FROM TEXT########################
-with open("data/service_data_mapping.json", 'r') as service:
+def get_arguments():
+
+        parser = argparse.ArgumentParser(prog = 'common_mash_reference.py', description= 'Search for all mash files and find the representative reference')
+
+        parser.add_argument('-j', '--json', dest="json", metavar="json info file", type=str, required=True, help='REQUIRED. json file with service information.')
+        parser.add_argument('-o', '--output', type=str, required=False, help='Output file to save results')
+
+        arguments = parser.parse_args()
+
+        return arguments
+
+    args = get_arguments()
+
+    input_dir = os.path.abspath(args.input_dir)
+
+
+def main():
+    #############FROM API###############################
+    #url= 'https://flavia.isciii.es/api/'
+    #response = requests.get(url)
+    #variables = json.loads(response.text)
+
+    ##################FROM TEXT########################
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    with open(args.json, 'r') as service:
+        try:
+            variables=json.load(service)
+        except ValueError:
+            print('Decoding JSON has failed')
+    #print(variables)
+    outputText = template.render(variables)
+    file = open(args.output + "/INFRES_"+variables['SERVICE_NUMBER']+".md","wb")
+    file.write(outputText.encode("utf-8"))
+    file.close()
+
+
+if __name__ == '__main__':
     try:
-        variables=json.load(service)
-    except ValueError:
-        print('Decoding JSON has failed')
-#print(variables)
-outputText = template.render(variables)
-file = open("results/INFRES_"+variables['SERVICE_NUMBER']+".md","wb")
-file.write(outputText.encode("utf-8"))
-file.close()
+        main()
+    except Exception as e:
+        logger.exception(e)
+        raise
