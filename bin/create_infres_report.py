62f4dd7226f205edd580ed29a09d1ae16a33b655
svarona <sarai.var.fer@gmail.com>
2022-01-18 11:12:32 +0100

    Added pymysql new version arguments

--- bin/create_infres_report.py
+++ bin/create_infres_report.py
@@ -97,7 +97,7 @@ def main():
 
         #############FROM API###############################
         # Open database connection
-        db = pymysql.connect("lamarck.isciii.es","iskylimsro","Rt9vddKTO-b","iSkyLIMS",6033)
+        db = pymysql.connect(host="lamarck.isciii.es",user="iskylimsro",password="Rt9vddKTO-b",database="iSkyLIMS",port=6033)
 
         # prepare a cursor object using cursor() method
         cursor = db.cursor()
